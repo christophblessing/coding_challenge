a8bcfd344dae1a2711ba9eee532466195b5e26c1
christophblessing <christophblessing@users.noreply.github.com>
2021-12-17 11:48:50 +0100

    Very basic component setup

--- src/Components/Startup/StartupList.tsx
+++ src/Components/Startup/StartupList.tsx
@@ -1,5 +1,23 @@
-import { Fragment, ReactElement } from "react";
+import { ReactElement, useEffect, useState } from "react";
+import { StartupHttpService } from "../../Http/Startup/Startup.http.service";
+import { Startup } from "../../Types/Startup";
 
 export default function StartupList(): ReactElement {
-  return <Fragment></Fragment>;
+  const [data, setData] = useState<Startup[]>([]);
+
+  useEffect(()=> {
+    StartupHttpService.getStartups().then(response => {
+      setData(response);
+    }).catch(reason => {
+      console.log(reason.response.data);
+    });
+  }, []);
+  
+  return (
+    <ul>
+      {
+        data.map(item => <li key={item.id}>{item.name}</li>)
+      }
+    </ul>
+  );
 }
