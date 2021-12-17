e258b69544f5d5120c0cd9f7a9c03cd08cb9dde1
christophblessing <christophblessing@users.noreply.github.com>
2021-12-17 11:32:09 +0100

    Implement getStartups method

--- src/Http/Startup/Startup.http.service.ts
+++ src/Http/Startup/Startup.http.service.ts
@@ -3,6 +3,13 @@ import { Startup, StartupDTO } from "../../Types/Startup";
 import StartupMapper from "./Startup.mapper";
 
 export class StartupHttpService {
+
+  public static async getStartups(): Promise<Startup[]> {
+    const response = await axios.get<StartupDTO[]>(`/api/startups`);
+    const startups = response.data.map(item => StartupMapper.map(item));
+    return startups;
+  }
+
   public static async getStartupById(id: string | number): Promise<Startup> {
     const response = await axios.get<StartupDTO>(`/api/startups/${id}`);
     return StartupMapper.map(response.data);
