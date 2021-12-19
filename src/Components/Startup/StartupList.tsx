ff26dd4838e0f722637b45a3a39c0aaa5cc9f4d6
christophblessing <christophblessing@users.noreply.github.com>
2021-12-19 12:53:54 +0100

    Add Card layout to StartupList

--- src/Components/Startup/StartupList.tsx
+++ src/Components/Startup/StartupList.tsx
@@ -1,23 +1,46 @@
 import { ReactElement, useEffect, useState } from "react";
 import { StartupHttpService } from "../../Http/Startup/Startup.http.service";
 import { Startup } from "../../Types/Startup";
+import { Box, Card, CardContent, Typography } from "@mui/material";
+
+const pipe = (
+  <Box component="span" sx={{ display: "inline-block", mx: "4px" }}>
+    |
+  </Box>
+);
 
 export default function StartupList(): ReactElement {
   const [data, setData] = useState<Startup[]>([]);
 
-  useEffect(()=> {
-    StartupHttpService.getStartups().then(response => {
-      setData(response);
-    }).catch(reason => {
-      console.log(reason.response.data);
-    });
+  useEffect(() => {
+    StartupHttpService.getStartups()
+      .then((response) => {
+        setData(response);
+      })
+      .catch((reason) => {
+        console.log(reason.response.data);
+      });
   }, []);
-  
+
   return (
     <ul>
-      {
-        data.map(item => <li key={item.id}>{item.name}</li>)
-      }
+      {data.map((item) => (
+        <Card key={item.id} sx={{ mb: 2 }}>
+          <CardContent>
+            <Typography variant="h5">{item.name}</Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              {item.dateFounded.getFullYear()}
+              {pipe}
+              {item.employees}
+              {pipe}
+              {item.totalFunding}
+              {pipe}
+              {item.currentInvestmentStage}
+            </Typography>
+            <Typography variant="body1">{item.shortDescription}</Typography>
+          </CardContent>
+        </Card>
+      ))}
     </ul>
   );
 }
